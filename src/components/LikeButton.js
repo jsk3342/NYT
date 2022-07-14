926a3646adb55162c04f64b27c6c0abf375f791c
jsk3342 <kss2460@naver.com>
2022-07-15 03:52:35 +0900

    Add: 좋아요 버튼 구현

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -1,11 +1,22 @@
 import styles from './LikeButton.module.css';
 import starEmpty from '../assets/star-empty.png';
+import starFill from '../assets/star-fill.png';
+import { useState } from 'react';
 
 const LikeButton = () => {
+  const [star, setStar] = useState(starEmpty);
+  const HandleStar = () => {
+    if (star === starEmpty) {
+      setStar(starFill);
+    } else setStar(starEmpty);
+  };
+
   return (
-    <button className={styles.likeButton} type="button">
-      <img src={starEmpty} alt="빈 좋아요 버튼" />
-    </button>
+    <div>
+      <button className={styles.likeButton} type="button" onClick={HandleStar}>
+        <img src={star} alt="빈 좋아요 버튼" />
+      </button>
+    </div>
   );
 };
 

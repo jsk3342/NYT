7895d256fa419c5f93aec4f171cb937c0bbb9c35
jsk3342 <kss2460@naver.com>
2022-07-15 00:34:39 +0900

    Add: axios 기사 제목 불러오기

--- src/components/Card/Title/Title.js
+++ src/components/Card/Title/Title.js
@@ -1,11 +1,35 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import styles from './Title.module.css';
 
+const key = 'bIiSLhG9fcmiMJTS0qNuG7btu0CxRE8r';
+let searchString = 'iphone';
+const URL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchString}&api-key=${key}`;
+
+// .data.response.docs[0].abstract
+
+// const showTitle = (title) => {
+//   console.log(title);
+//   return title;
+// };
+
 const Title = () => {
-  return (
-    <h2 className={styles.title}>
-      국방부 “北, 화성-17 실패 만회하려 영상 짜깁기… 성공 조작”
-    </h2>
-  );
+  const [result, setResult] = useState('');
+  console.log(result);
+  useEffect(() => {
+    async function GetTitle() {
+      try {
+        const res = await axios.get(URL);
+        const serchResult = res.data.response.docs[0].abstract;
+        console.log(serchResult);
+        setResult(serchResult);
+      } catch (error) {
+        console(error);
+      }
+    }
+    GetTitle();
+  }, []);
+  return <h2 className={styles.title}>{result}</h2>;
 };
 
 export default Title;

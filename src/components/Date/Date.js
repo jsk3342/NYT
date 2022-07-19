6425c4ef6d90908cfba68dabbecec599747c1fc2
jsk3342 <kss2460@naver.com>
2022-07-19 10:50:51 +0900

    Add: 데이터피커 트라이

--- src/components/Date/Date.js
+++ src/components/Date/Date.js
@@ -1,10 +1,35 @@
 import styles from './Date.module.css';
+import ReactDatePicker from 'react-datepicker';
+import { useState } from 'react';
 
 function Date() {
+  // let date = '';
+  // console.log(date);
+  // const [startDate, setStartDate] = useState(date);
+  // const [endDate, setEndDate] = useState(date);
+  // const CustomInput = ({ value, onClick }) => (
+  //   <button className="example-custom-input" onClick={onClick}>
+  //     {value}
+  //   </button>
+  // );
   return (
     <section>
       <label className={styles.title}>날짜</label>
       <input className={styles.dateInput} placeholder="날짜를 선택해주세요" />
+      {/* <ReactDatePicker
+        selected={startDate}
+        selectsStart
+        maxDate={date}
+        customInput={<CustomInput />}
+      />
+      <ReactDatePicker
+        selected={endDate}
+        selectsEnd
+        startDate={startDate}
+        minDate={startDate}
+        maxDate={date}
+        customInput={<CustomInput />}
+      /> */}
     </section>
   );
 }
